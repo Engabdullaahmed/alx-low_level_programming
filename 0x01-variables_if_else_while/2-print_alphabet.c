e2f8636001560d4ee53b3877d0c3e760449d2bc4
Abdulla <ahmedmoustafaabdelwahed@gmail.com>
2023-03-23 23:03:26 -0700

    a

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,23 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
-* print_remaining_days - takes a date and prints how many days are
-* left in the year, taking leap years into account
-* @month: month in number format
-* @day: day of month
-* @year: year
-* Return: void
-*/
+ * main- Entry point
+ * @void: Null value
+ *
+ * Description: Print alphabet lowercase
+ * Return: Zero value
+ */
 
 int main(void)
 {
-char x;
+	char c;
 
-while(x >= 'a' && x <= 'z'){
-x++ ;
-putchar(x) ;
-putchar("\n") ;
-}
-
-return (0);
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
 }
