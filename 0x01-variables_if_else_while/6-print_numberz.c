7af2a7339869438de27d57d837814448f94c7990
Abdulla <ahmedmoustafaabdelwahed@gmail.com>
2023-03-23 23:22:22 -0700

    a

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ * @void: Null value
+ *
+ * Description: Print numbers from 0 to 9
+ * Return: Zero value
+ */
+
+int main(void)
+{
+int x;
+
+for (x = '0'; x <= '9'; x++)
+putchar(x);
+putchar('\x');
+return (0);
+}
