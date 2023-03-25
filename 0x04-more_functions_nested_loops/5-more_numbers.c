2a36397c9e6081d2439a375611619a63bed4fcfe
Abdulla <ahmedmoustafaabdelwahed@gmail.com>
2023-03-24 18:16:11 -0700

    a

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - print more numbers
+ */
+
+void more_numbers(void)
+{
+
+int x;
+int cnt = 0;
+
+while (cnt < 10)
+{
+while (x <= 14)
+{
+_putchar(1);
+cnt++;
+}
+_putchar('\n');
+}
