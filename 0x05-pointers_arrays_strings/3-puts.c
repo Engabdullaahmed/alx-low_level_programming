fd4603f56d34808fd6a65f2dba57db915390ede3
Abdulla <ahmedmoustafaabdelwahed@gmail.com>
2023-03-27 18:57:37 -0700

    a

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+
+while (*s != '\0')
+{
+_putchar(*s++);
+}
+_putchar('\n')
+}
