bacf27eb7ba04a2d0576e904e0b93c390bdc0f47
Abdulla <ahmedmoustafaabdelwahed@gmail.com>
2023-04-05 17:54:39 -0700

    a

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - which print revese string
+ * @s: pointer to string
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+if (*s != '\0')
+{
+_print_rev_recursion(s + 1);
+printf("%c", *s);
+}
+}
