7a4ecdb524530f297d8bbaca152f50cb988b9e1a
Abdulla <ahmedmoustafaabdelwahed@gmail.com>
2023-04-09 15:04:35 -0700

    qwqw

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_memcpy - copy memory area
+ *@dest: store memory
+ *@src: copied memory
+ *@n: number of bytes
+ *Return: copied memory
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+for (int q = 0, int p = n; q < i; q++)
+{
+dest[q] = src[q];
+n--;
+}
+return (dest);
+}
