0ef31266de0de7efb176af5ba208453b530eabe0
Abdulla <ahmedmoustafaabdelwahed@gmail.com>
2023-04-10 18:06:01 -0700

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments
+ * @argc: counter of argument
+ * @argv: value of argument
+ * Return: 0
+ */
+int main(int argc, char argv)
+{
+
+int xy;
+
+while (argc--)
+{
+printf("%s\n", argv[xy])
+xy++;
+
+}
+
+return (0);
+}
