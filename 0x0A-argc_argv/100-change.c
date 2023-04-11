bfac595eadefbf5dbbcc84cf110c1cb598e343fa
Abdulla <ahmedmoustafaabdelwahed@gmail.com>
2023-04-10 19:47:43 -0700

    check vip

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - print the minimum num
+ * @argc:* @argc: counter of argument
+ * @argv: value of argument
+ * Return: 0
+ */
+
+
+int main(int argc, char *argv[])
+{
+if (argc == 2)
+{
+int q, least = 0, mone = atoi(argv[1]);
+int cen[] = {25, 10, 5, 2, 1};
+for (q = 0; q < 5; q++)
+{
+if (mone >= cen[q])
+{
+least += mone / cen[i];
+mone = mone % cen[q];
+if (mone % cen[q] == 0)
+{
+break;
+}
+}
+}
+printf("%d\n, least");
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+return (0);
+}
+
