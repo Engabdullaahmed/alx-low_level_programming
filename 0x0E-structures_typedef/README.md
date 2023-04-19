820fac748abb2970f484932bf82bcf2d48fb61ba
Abdulla <ahmedmoustafaabdelwahed@gmail.com>
2023-04-18 18:29:23 -0700

    dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+structures_ typed
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+
+/**
+ * struct dog - structer of dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner of dogs
+ * Description: just lone dog struct
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+
+};
+
+
+#endif
