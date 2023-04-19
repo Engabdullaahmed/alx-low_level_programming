9db756cfc46dc9f1d1b96b93b37a75eea2a7e1b0
Abdulla <ahmedmoustafaabdelwahed@gmail.com>
2023-04-18 19:03:38 -0700

    1_init_dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+
+/**
+ * init_dog - int dog
+ * @d: dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d == 0)
+
+d = malloc(sizeof(struct dog));
+
+d->name = name;
+d->age = age;
+d->owner = owner;
+
+}
