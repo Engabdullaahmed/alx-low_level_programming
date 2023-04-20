daad21e8d512e8630c653d96c0f0e9baa358000a
Abdulla <ahmedmoustafaabdelwahed@gmail.com>
2023-04-20 07:38:49 -0700

    calculter

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,58 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * op_add - adds a and b
+ * @a: first num
+ * @b: second num
+ * Return: a + b
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_mul - mul a and b
+ * @a: first num
+ * @b: second num
+ * Return: a * b
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * op_div - div a and b
+ * @a: first num
+ * @b: second num
+ * Return: a / b
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ * op_sub - sub a and b
+ * @a: first num
+ * @b: second num
+ * Return: a - b
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * op_mod - mod a and b
+ * @a: first num
+ * @b: second num
+ * Return: a % b
+ */
+int op_mod(int a, int b)
+{
+return (a % b);
+}
