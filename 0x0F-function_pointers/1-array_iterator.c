556a8b680905e5e274982b6e9622683fe976c860
Abdulla <ahmedmoustafaabdelwahed@gmail.com>
2023-04-20 04:40:00 -0700

    argument of array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - that give each paramter of arr
+ * @array: pointer of arr
+ * size: unsign long long
+ * @action: refer  pointer of function
+ * Return: void
+ */
+
+#include <stdio.h>
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+size_t i;
+if (array != NULL && action != NULL)
+for (i = 0; i < size; i++)
+{
+action(array[i]);
+}
+}
