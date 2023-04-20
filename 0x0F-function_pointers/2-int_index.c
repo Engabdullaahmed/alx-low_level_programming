8a6befb09a2398be774072e3ad90873cfee6f46d
Abdulla <ahmedmoustafaabdelwahed@gmail.com>
2023-04-20 05:29:23 -0700

    find int

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+
+/**
+ * int_index - give the index of the first element
+ * @array: array
+ * @size: int
+ * @cmp: pointer to function
+ * Return: 0
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+if (array && cmp)
+{
+for (i = 0; i < size ; i++)
+if (cmp(array[i]))
+{
+return (i);
+}
+return (-1);
+}
+}
