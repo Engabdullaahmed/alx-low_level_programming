4139df9bfe3fefbebff94234f76a749245acf014
Abdulla <ahmedmoustafaabdelwahed@gmail.com>
2023-04-20 19:02:12 -0700

    sum of arg

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,10 +1,11 @@
-#ifndef VARACIC_H
-#define VARACIC_H
+#ifndef _VARACIC_H
+#define _VARACIC_H
 
 #include <stdio.h>
 #include <stdarg.h>
 
 
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
