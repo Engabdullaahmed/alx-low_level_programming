2be512db9a19d71750d006a094b3c55dd8b16a90
Abdulla <ahmedmoustafaabdelwahed@gmail.com>
2023-04-20 19:52:26 -0700

    so difficult

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,90 +1,47 @@
-#include <stdarg.h>
 #include <stdio.h>
 #include "variadic_functions.h"
 
 /**
- * print_c - print char
- * @c: char
- * Return: 0
- */
-void print_c(va_list c)
-{
-printf("%c", va_arg(c, int));
-}
-
-/**
- * print_s - prints string
- * @s: string
- * Return: 0
- */
-void print_s(va_list s)
-{
-char *str = va_arg(s, char *);
-
-if (str == NULL)
-str = "(nil)";
-printf("%s", str);
-}
-
-/**
- * print_i - prints an int
- * @i: int to print
- *
- * Return: void
- */
-void print_i(va_list i)
-{
-printf("%d", va_arg(i, int));
-}
-
-/**
- * print_f - prints a float
- * @f: float to print
- *
- * Return: 0
- */
-void print_f(va_list f)
-{
-printf("%f", va_arg(f, double));
-}
-
-/**
  * print_all - print anything
- * @format: list of argument
+ * @format: list of types
  * Return: 0
  */
+
 void print_all(const char * const format, ...)
 {
-unsigned int i, j;
-print_t p[] = {
-{"c", print_c},
-{"s", print_s},
-{"i", print_i},
-{"f", print_f},
-{NULL, NULL}
-};
+	int poe = 0;
+	char *str, *sep = "";
 
-va_list valist;
-char *separator = "";
+	va_list list;
 
-va_start(valist, format);
-i = 0;
-while (format && format[i])
-{
-j = 0;
-while (p[j].t != NULL)
-{
-if (*(p[j].t) == format[i])
-{
-printf("%s", separator);
-p[j].f(valist);
-separator = ", ";
-break;
-}
-j++;
-}
-i++;
-}
-va_end(valist);
-printf("\n");
+	va_start(list, format);
+
+	while (format && format[poe])
+	{
+		switch (format[poe])
+		{
+			case 'c':
+				printf("%s%c", sep, va_arg(list, int));
+				break;
+			case 'i':
+				printf("%s%d", sep, va_arg(list, int));
+				break;
+			case 'f':
+				printf("%s%f", sep, va_arg(list, double));
+				break;
+			case 's':
+				str = va_arg(list, char *);
+				if (!str)
+					str = "(nil)";
+				printf("%s%s", sep, str);
+				break;
+			default:
+				poe++;
+				continue;
+		}
+		sep = ", ";
+		poe++;
+	}
+	printf("\n");
+	va_end(list);
 }
