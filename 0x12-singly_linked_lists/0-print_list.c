3d5c1aa41e4d3c06dc874b121eddc5124d9a1e9d
Eng Abdulla <ahmedmoustafaabdelwahed@gmail.com>
2023-04-27 18:50:54 -0700

    task 0 of linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,38 @@
+#include "list.h"
+
+/**
+ * _strlen - len of string
+ * @s: the string
+ * Return: len of str
+ */
+
+int _strlen(char *s)
+{
+int x = 0;
+
+if (s == NULL)
+return (0);
+
+while (*s++)
+x++;
+return (x);
+}
+
+
+/**
+ * print_list - linked list
+ * @h: first node the declare from pointer
+ * Return: list size
+ */
+
+size_t print_list(const list_t *h)
+{
+
+while (h)
+{
+printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+h = h->next;
+x++;
+}
+return (x);
+}
