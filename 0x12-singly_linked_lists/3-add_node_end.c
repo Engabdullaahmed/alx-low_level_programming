ca7590444ac1f8902dafa25645e4529d224e4b8d
Eng Abdulla <ahmedmoustafaabdelwahed@gmail.com>
2023-04-27 19:42:59 -0700

    done task four or 3

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,52 @@
+#include "lists.h"
+/**
+ * _strlen - len of string
+ * @str: string
+ * Return: leng of string
+ */
+
+unsigned int _strlen(char *str)
+{
+unsigned int i;
+
+for (i = 0; str[i]; i++)
+;
+return (i);
+}
+
+/**
+ * add_node_end - adds a new node
+ * @head: pointer to linked list
+ * @str: string
+ * Return: pointer of new node
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+list_t *new, *tmp;
+
+if (!str)
+return (NULL);
+new = malloc(sizeof(list_t));
+
+if (!new)
+return (NULL);
+new->str = strdup(str);
+if (new->str == NULL)
+{
+free(new);
+return (NULL);
+}
+new->len = _strlen(new->str);
+new->next = NULL;
+if (*head == NULL)
+{
+*head = new;
+return (new);
+}
+tmp = *head;
+while (tmp->next)
+tmp = tmp->next;
+tmp->next = new;
+return (new);
+}
