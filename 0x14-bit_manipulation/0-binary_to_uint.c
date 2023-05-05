526f3b63c2e4bf1b8f6fa6d93ef9e5903398390f
Eng Abdulla <ahmedmoustafaabdelwahed@gmail.com>
2023-05-05 12:49:46 -0700

    nice problem

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,17 +8,17 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-int x ;
+int x;
 unsigned int value_int = 0;
 
 if (b == NULL)
-return 0;
+return (0);
 
 for (x = 0; b[x]; x++)
 {
 
 if (b[x] != '0' && b[x] != '1')
-return 0;
+return (0);
 
 value_int = 2 * value_int + (b[x] - '0');
 }
