f60f776a0a85906f93d03edbb4244fb87a945bb0
Eng Abdulla <ahmedmoustafaabdelwahed@gmail.com>
2023-05-05 16:52:02 -0700

    clear bit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * set_bit - sets the value of bit
+ * @n: pointer  num
+ * @index: index of the bit to set (starting from 0)
+ * Return: 1 if  work or -1 if an error
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+
+
+if (index > 63)
+return (-1);
+
+*n = (~(1UL << index) & *n);
+return (1);
+}
