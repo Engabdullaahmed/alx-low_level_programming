1d6129d002d544bb35cde33c9470a2628587de43
Eng Abdulla <ahmedmoustafaabdelwahed@gmail.com>
2023-05-05 12:47:17 -0700

    nice problem

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/*
+ * binary_to_uint - convert from binary to int
+ * @b: pointer which string
+ * Return: give int from binary
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+int x ;
+unsigned int value_int = 0;
+
+if (b == NULL)
+return 0;
+
+for (x = 0; b[x]; x++)
+{
+
+if (b[x] != '0' && b[x] != '1')
+return 0;
+
+value_int = 2 * value_int + (b[x] - '0');
+}
+return (value_int);
+}
