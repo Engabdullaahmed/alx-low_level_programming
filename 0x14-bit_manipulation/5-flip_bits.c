9cfa4dd7afbb14f13a36650b66354a47e9e6dc7b
Eng Abdulla <ahmedmoustafaabdelwahed@gmail.com>
2023-05-05 17:55:51 -0700

    sdfjklsjiowej

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -11,7 +11,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 
 unsigned long int counter = 0;
-unsigned long int xor = n ^ m; 
+unsigned long int xor = n ^ m;
 int x;
 int current;
 
