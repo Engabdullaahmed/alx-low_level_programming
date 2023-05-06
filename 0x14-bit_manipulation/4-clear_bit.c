a92022eabe7049117682cc23abf3d44e1f5b1d2a
Eng Abdulla <ahmedmoustafaabdelwahed@gmail.com>
2023-05-05 17:34:47 -0700

    soen df;dslk

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * set_bit - sets the value of bit
+ * clear_bit - sets the value of bit
  * @n: pointer  num
  * @index: index (starting from 0)
  * Return: 1 if  work  -1 if there error
  */
 
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 
 
