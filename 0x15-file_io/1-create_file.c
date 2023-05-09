c6385a674fdf0ee04eb5b4a844d3b59563093c3c
Eng Abdulla <ahmedmoustafaabdelwahed@gmail.com>
2023-05-08 17:43:20 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,7 @@
  * @filename: pointer name of file
  * @text_content: string pointer
  * Return: fail -1
- */ 
+ */
 
 
 int create_file(const char *filename, char *text_content)
@@ -24,7 +24,7 @@ return (-1);
 
 if (text_content != NULL)
 {
-for(len = 0; text_content[len]; len++);
+for (len = 0; text_content[len]; len++)
 }
 
 
